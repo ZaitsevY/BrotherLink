34a66ebaaeaccfeb457161d0b4db113c53e856e8
ZaitsevY <ZaitsevY@gmail.com>
2016-03-03 18:03:23 +0300

    Updated for esp8266web 0.5.9a project

--- Source/brother_link.c
+++ Source/brother_link.c
@@ -21,99 +21,120 @@
 //-------------------------------------------------------------------------------
 err_t tcp2uart_write(uint8 *pblk, uint16 len)
 {
-	TCP_SERV_CONN *conn = tcp2uart_conn;
-	if (conn == NULL) return ERR_CONN;
+	uint8* buffer = uart_drv.uart_rx_buf;
+	if (buffer == NULL) return ERR_CONN;
 
-	// Нельзя так вызывать, ибо там указатель на буфер
-	// после отправки переставляется на конец этого pblk,
-	// который, по результату, пальцем тычет в анус негра.
-	// return tcpsrv_int_sent_data(conn, pblk, len);
+	// Going to touch buffers usually filled by UART
+	// interrupt handler: disable interrupts temporary.
+	ETS_INTR_LOCK();
 
-	// Надо: засунуть данные в буфер отправки соединения pbufo
-	// и уж тогда и вызывать tcpsrv_int_sent_data - списать с
-	// tcp2uart.c:loading_rx_buf(). Во гемор-то...
-
-	ETS_UART_INTR_DISABLE();
-	conn->flag.user_flg1 = 1;
-
-	err_t result;
-	if (conn->flag.wait_sent) {
-		// LwIP занят отсылкой, буфер не трогать
-		result = ERR_INPROGRESS;
-	} else {
-		uint16 size = len;
-		uint8 *pend = conn->pbufo + conn->sizeo;
-		while((conn->ptrtx + conn->cntro) < pend && size) {
-			conn->ptrtx[conn->cntro++] = *pblk;
-			size--;  pblk++;
-		}
-		result = tcpsrv_int_sent_data(conn, conn->pbufo, conn->cntro);
-		if (result == ERR_OK) {
-			// удалить из буфера переданные данные
-			if(conn->ptrtx != conn->pbufo && conn->cntro != 0)
-				os_memcpy(conn->pbufo, conn->ptrtx, conn->cntro);
-			conn->ptrtx = conn->pbufo; // указатель на начало не переданных данных (начало буфера)
-		}
+	// Number of bytes in buffer so far
+	uint32 length = uart_drv.uart_rx_buf_count;
+	if (length >= UART_RX_BUF_MAX
+			|| uart_drv.uart_nsnd_buf_count >= UART_RX_BUF_MAX
+			|| len > UART_RX_BUF_MAX - length) {
+		// No space in buffer
+		ETS_INTR_UNLOCK();
+		return ERR_INPROGRESS;
 	}
 
-	conn->flag.user_flg1 = 0;
-	ETS_UART_INTR_ENABLE();
+	// Copy data into buffer
+	while (length < UART_RX_BUF_MAX && len--)
+		uart_drv.uart_rx_buf[length++] = *pblk++;
+	uart_drv.uart_rx_buf_count = length;
 
-	return result;
+	// Number of non-delivered bytes
+	length -= uart_drv.uart_out_buf_count;
+	if (length) {
+		// Request TCP2UART to deliver to TCP peer
+		system_os_post(UART_TASK_PRIO, UART_RX_CHARS, length);
+		uart_drv.uart_nsnd_buf_count += length;
+		uart_drv.uart_out_buf_count = uart_drv.uart_rx_buf_count;
+	}
+
+	// Finished
+	ETS_INTR_UNLOCK();
+	return ERR_OK;
 }
 
 #define BUTTON_GPIO 4	// GPIO4
 #define BUTTON_TASK_PRIO (USER_TASK_PRIO_1)
 #define BUTTON_TASK_QUEUE_LEN 2
-const uint32 BUTTON_CLICK_DELAY = 200*1000;	// Next click not earlier than after, ms
-const GPIO_INT_TYPE BUTTON_INTR = GPIO_PIN_INTR_POSEDGE;
+static const uint32 BUTTON_CLICK_DELAY = 400*1000;	// Next click not earlier than after, ms
+static const GPIO_INT_TYPE BUTTON_INTR = GPIO_PIN_INTR_NEGEDGE;
 
 os_event_t _queue[BUTTON_TASK_QUEUE_LEN] DATA_IRAM_ATTR;
 uint32 _click DATA_IRAM_ATTR = 0;
 uint32 _count DATA_IRAM_ATTR = 0;
 
+/**
+ * GPIO interrupt handler.
+ */
 static void intr_brother_link(void) {
+	// Read GPIO status
 	uint32 gpio_status = GPIO_STATUS;
+	// Acknowledge GPIO interrupt
 	GPIO_STATUS_W1TC = gpio_status;
+	// Is is from mine reed switch?
 	if (gpio_status & (1 << BUTTON_GPIO)) {
+		// Schedule task execution
 		system_os_post(BUTTON_TASK_PRIO, 0, 0);
+		// Call me again next time
 	    gpio_pin_intr_state_set(BUTTON_GPIO, BUTTON_INTR);
 	}
 }
 
+/**
+ * Reed switch task handler.
+ * Send "click" to DesignaKnit.
+ */
 static void ICACHE_FLASH_ATTR task_brother_link(os_event_t *e)
 {
 	uint32 clock = system_get_time();
-	//	if (clock < _click) {
-	//		// System time overflow?
-	//		_click = 0;
-	//	}
-	if (e->par || clock >= _click) {	// Forced? or time to click?
-		_click = clock + BUTTON_CLICK_DELAY;	// Next click not sooner
-		uint8 letter = (_count % 10) + '0';
-		if (ERR_INPROGRESS == tcp2uart_write(&letter, sizeof(letter)))
+	if (clock < _click) {
+		// System time overflow
+		_click = 0;
+	}
+	if (e->par || clock >= _click + BUTTON_CLICK_DELAY) {	// Forced? or time to click?
+		uint8 letter = (_count % 10) + '0';		// Actually just any symbol works
+		if (ERR_INPROGRESS == tcp2uart_write(&letter, sizeof(letter))) {
+			// Could not send, retry again later (forced)
 			system_os_post(BUTTON_TASK_PRIO, 0, 1);
-		else
+		} else {
+			// Fine, just remember that shiny moment
+			_click = clock;
 			_count++;
+		}
 	}
 }
 
+/**
+ * BrotherLink initialization.
+ */
 void ICACHE_FLASH_ATTR init_brother_link(void)
 {
- 	os_printf("Brother link init...\n");
+ 	os_printf("\nBrotherLink...\n");
+
+ 	// Store initialization time
 	_click = system_get_time();
+
+	// Register event queue
 	system_os_task(task_brother_link, BUTTON_TASK_PRIO, _queue, BUTTON_TASK_QUEUE_LEN);
+
+	// Configure GPIO for reed switch
 	uint32 pins_mask = (1 << BUTTON_GPIO);
-	gpio_output_set(0, 0, 0, pins_mask);	// настроить GPIOx на ввод
-	set_gpiox_mux_func_ioport(BUTTON_GPIO);	// установить функцию GPIOx в режим порта i/o
-	ETS_GPIO_INTR_ATTACH(intr_brother_link, NULL);
-	gpio_pin_intr_state_set(BUTTON_GPIO, BUTTON_INTR);
-	// разрешить прерывания GPIOs
-	GPIO_STATUS_W1TC = pins_mask;
+	ETS_GPIO_INTR_DISABLE();
+	gpio_output_set(0, 0, 0, pins_mask);	// configure pin for input
+	set_gpiox_mux_func_ioport(BUTTON_GPIO);	// set pin function to GPIO
+	ETS_GPIO_INTR_ATTACH(intr_brother_link, NULL);	// attach interrupt handler
+	gpio_pin_intr_state_set(BUTTON_GPIO, BUTTON_INTR);	// to fire on condition
+	GPIO_STATUS_W1TC = pins_mask;			// clear any GPIO interrupts
 	ETS_GPIO_INTR_ENABLE();
 }
 
-// Custom initialization sequence implementation
+/**
+ * Custom initialization sequence implementation.
+ */
 void ICACHE_FLASH_ATTR custom_init() {
 	init_brother_link();
 }
